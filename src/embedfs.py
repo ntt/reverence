20ea65a084d9c95d0b526c75513811fdc151c041
unknown <Entity@Rarity.(none)>
2012-06-26 18:05:48 +0200

    Added virtual file class for EmbedFS.

--- src/embedfs.py
+++ src/embedfs.py
@@ -1,6 +1,6 @@
 """EmbedFS manager (handles .stuff files).
 
-Copyright (c) 2003-2011 Jamie "Entity" van den Berge <jamie@hlekkir.com>
+Copyright (c) 2003-2012 Jamie "Entity" van den Berge <jamie@hlekkir.com>
 
 This code is free software; you can redistribute it and/or modify
 it under the terms of the BSD license (see the file LICENSE.txt
@@ -8,12 +8,14 @@ included with the distribution).
 """
 
 
-import cStringIO
 import struct
 import glob
 import os
 
-idString = "EmbedFs 1.0"
+from ._blue import _VirtualFile
+
+
+_idString = "EmbedFs 1.0"
 
 class EmbedFS(object):
 	"""Manages a single EmbedFS file."""
@@ -30,9 +32,10 @@ class EmbedFS(object):
 
 		# open file and check
 		f = open(fileName, "rb")
+
 		self.name = fileName
-		f.seek(-len(idString)-1, 2)
-		if idString != f.read(len(idString)):
+		f.seek(-len(_idString)-1, 2)
+		if _idString != f.read(len(_idString)):
 			raise RuntimeError("Invalid id string in EmbedFS file")
 		f.seek(0)
 		self.numFiles, = struct.unpack("<L", f.read(4))
@@ -40,7 +43,7 @@ class EmbedFS(object):
 		# read directory
 		for i in range(self.numFiles):
 			length, nameLength = struct.unpack("<2L", f.read(8))
-			name = f.read(nameLength+1).replace('\\', '/').strip("\0")
+			name = f.read(nameLength+1).replace('\\', '/').rstrip("\0")
 			addf(name)
 			addl(length)
 			files[name.lower()] = i
@@ -51,7 +54,10 @@ class EmbedFS(object):
 			addo(offset)
 			offset += length
 
-		self.stream = f
+		# keep the stuff file open to prevent changes to it (patches?)
+		# invalidating the offsets/lengths.
+		self._filehandle = f
+
 
 	def open(self, name):
 		f = self._open(name)
@@ -68,14 +74,13 @@ class EmbedFS(object):
 	def __len__(self):
 		return len(self.filenames)
 
-	def _open(self, name):
+	def _open(self, name, mode="rb", buffering=-1):
 		# internal open.
 		ix = self.files.get(name.lower(), -1)
 		if ix == -1:
 			return None
-		self.stream.seek(self.offsets[ix])
-		return cStringIO.StringIO(self.stream.read(self.lengths[ix]))
 
+		return _VirtualFile(self.name, mode, buffering, self.offsets[ix], self.lengths[ix])
 
 
 class EmbedFSDirectory(object):
@@ -93,4 +98,6 @@ class EmbedFSDirectory(object):
 			f = efs._open(name)
 			if f:
 				return f
-		raise IndexError("File not found")
+		raise IndexError("File not found: '%s'" % name)
+
+

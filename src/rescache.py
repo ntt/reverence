540a3e8ec023663a3803c5b6812964a2f6927c2f
ntt <jamie@hlekkir.com>
2015-03-30 02:09:57 +0200

    Better download-on-demand with strict checks on file properties reported in resfileindex.txt

--- src/rescache.py
+++ src/rescache.py
@@ -13,6 +13,7 @@ import csv
 import os.path
 import urllib2
 import zlib
+import hashlib
 
 from .config import _memoize
 
@@ -35,14 +36,13 @@ class ResourceCache(object):
 
 
 	def open(self, name):
-		name = name.lower()
 		try:
-			resPath = self._nameMap[name.lower()]
+			_, resPath, file_hash, file_size, compressed_size = self._index[name.lower()]
 		except KeyError:
 			raise IndexError("File not in resfileindex: '%s'" % name)
 
 		fullPath = os.path.join(self._sharedCachePath, 'ResFiles', resPath)
-		
+
 		if _useragent is None:
 			return open(fullPath, mode='rb')
 		else:
@@ -51,48 +51,62 @@ class ResourceCache(object):
 			except IOError:
 				# file not present. download it.
 				if not os.path.exists(fullPath):
-					self._download(resPath, fullPath)
+					self._download(resPath, fullPath, file_hash, file_size, compressed_size)
 					return open(fullPath, mode='rb')
 
 
 	@_memoize
-	def _nameMap(self):
-		# Format:
-		# {resource file name: path name relative to res cache root}
-		nameMap = {}
+	def _index(self):
+		d = {}
 		indexPath = os.path.join(self._evePath, 'resfileindex.txt')
 		with open(indexPath) as csvFile:
 			for row in csv.reader(csvFile):
-				resPath = row[0].lower()
-				filePath = row[1]
-				nameMap[resPath] = filePath
-		return nameMap
+				d[row[0].lower()] = row
+		return d
 
 		
-	def _download(self, resPath, fullPath):
+	def _download(self, resPath, fullPath, file_hash, file_size, compressed_size):
 		# make sure the folder exists.
 		folder = os.path.dirname(fullPath)
 		if not os.path.exists(folder):
 			os.makedirs(folder)
 
+		tempfilename = fullPath + ".part"
+			
 		# now try all download servers.
 		for server in _DOWNLOAD_SERVERS:
 			try:
 				req = urllib2.Request(server+resPath, None, headers={"User-Agent": _useragent})
 				response = urllib2.urlopen(req)
-				
-				with open(fullPath, "wb") as f:
+				with open(tempfilename, "wb") as f:
 					d = zlib.decompressobj(zlib.MAX_WBITS|32)  # gzip mode
+					m = hashlib.md5()
+					csize = 0
 					while True:
 						buffer = response.read(_BLOCKSIZE)
 						if not buffer:
 							break
-						f.write(d.decompress(buffer))
+						csize += len(buffer)
+						buffer = d.decompress(buffer)
+						m.update(buffer)
+						f.write(buffer)
+					fsize = f.tell()
+					
+				# as we might be downloading directly to EVE's shared
+				# cache, do strict tests to see if what we asked for is
+				# what we actually got.
+				if str(csize) != compressed_size or str(fsize) != file_size or m.hexdigest() != file_hash:
+					raise RuntimeError("Download failed - file %s does not match index." % fullPath)
+
+				# rename file to its proper cache name.
+				os.rename(tempfilename, fullPath)
 				return
 
 			except Exception, e:
-				pass
-			raise e
+				# delete the temporary file, it's probably useless anyway.
+				if os.path.exists(tempfilename):
+					os.unlink(tempfilename)
+
+		raise e
 
 
-				
